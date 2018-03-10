ea19f16be1f63b12541b58644638ef9ea941873b
Fábio Gouveia <hpgroods@gmail.com>
2018-03-10 18:39:49 +0000

    initial commit

---
+++ app/src/main/java/com/example/android/lastmusicalstructure/utils/QueryUtils.java
@@ -0,0 +1,11 @@
+package com.example.android.lastmusicalstructure.utils;
+
+/**
+ * The {@link QueryUtils} class represents a helper to query application data.
+ *
+ * @author Fábio Gouveia
+ * @version 1.0
+ */
+
+public class QueryUtils {
+}
