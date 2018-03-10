673317b440a7f4544dfe2cc2d7d5b59545f3e32b
Fábio Gouveia <hpgroods@gmail.com>
2018-03-10 19:11:52 +0000

    initial commit

---
+++ app/src/main/java/com/example/android/lastmusicalstructure/utils/HttpUtils.java
@@ -0,0 +1,12 @@
+package com.example.android.lastmusicalstructure.utils;
+
+/**
+ * The {@link HttpUtils} class is responsible to manage all about http requests
+ *
+ * @author Fábio Gouveia
+ * @version 1.0
+ */
+
+public class HttpUtils {
+
+}
