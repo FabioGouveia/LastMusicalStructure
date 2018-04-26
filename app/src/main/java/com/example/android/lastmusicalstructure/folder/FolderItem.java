cb00663e9a20668e86e06d68a6330a8d0e47c012
Fábio Gouveia <hpgroods@gmail.com>
2018-04-26 05:31:56 +0100

    feat: add folder item interface
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

---
+++ app/src/main/java/com/example/android/lastmusicalstructure/folder/FolderItem.java
@@ -0,0 +1,12 @@
+package com.example.android.lastmusicalstructure.folder;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Parcelable;
+
+public interface FolderItem extends Parcelable {
+    long getId();
+
+    String getName();
+
+    BitmapDrawable getImage();
+}
